f27406d1d87d691778a95151f4ccc4e2fdcbc54f
Sameen Mirza <mirzasameen63@gmail.com>
2026-01-30 20:18:21 +0530

    second commit

--- src/ourPro/ourClass.java
+++ src/ourPro/ourClass.java
@@ -4,6 +4,7 @@ public class ourClass {
 	public static void main(String[] args) {
 		System.out.println("our class");
 		System.out.println("your class");
+		System.out.println("your class");
 	}
 
 }
