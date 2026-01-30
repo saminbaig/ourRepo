8271c98dfd39016b77c59ed70ca8eb4768eb1d9e
saminbaig <msameen763@gmail.com>
2026-01-30 20:31:08 +0530

    Update ourClass.java

--- src/ourPro/ourClass.java
+++ src/ourPro/ourClass.java
@@ -2,9 +2,7 @@ package ourPro;
 
 public class ourClass {
 	public static void main(String[] args) {
-		System.out.println("our class");
-		System.out.println("your class");
-		System.out.println("your class");
+	
 	}
 
 }
